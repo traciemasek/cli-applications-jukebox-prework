5f8f7f820fece321695fb6929e775155838d3dfd
Tracie Masek <traciemasek@gmail.com>
2019-06-30 12:14:17 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -43,7 +43,8 @@ def play(songs)
   if songs[user_response.to_i-1]
     puts "Playing #{songs[user_response.to_i-1]}"
   elsif songs.include?(user_response)
-    puts "Playing #{songs[songs.index(user_response)]}"
+    puts "#{songs.find{|song| song == response}}"
+    #puts "Playing #{songs[songs.index(user_response)]}"
   else 
     puts "Invalid input, please try again"
   end
