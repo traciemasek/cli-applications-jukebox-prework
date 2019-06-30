3b81071f604f1514f1ae38e9f873abfba9ae6ecd
Tracie Masek <traciemasek@gmail.com>
2019-06-30 11:54:58 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -42,6 +42,7 @@ def play(songs)
   user_response = gets.chomp
   if songs[user_response.to_i-1]
     puts "Playing #{songs[user_response.to_i-1]}"
+  elsif songs.include?(user_response)
   else 
     puts "Invalid input, please try again"
   end
