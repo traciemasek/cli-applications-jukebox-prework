f2b855f4e90fd76c9c8808b90666c6165c859d93
Tracie Masek <traciemasek@gmail.com>
2019-06-30 12:02:40 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -39,7 +39,7 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  user_response = gets.chomp
+  user_response = gets.strip
   if songs[user_response.to_i-1]
     puts "Playing #{songs[user_response.to_i-1]}"
   elsif songs.include?(user_response)
